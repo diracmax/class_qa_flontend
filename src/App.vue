651a1cf20e6e4134f570329822dc5e7edfd402fa
diracmax <you@example.com>
2022-05-11 10:25:59 +0900

    ヘッダー作成

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+  <my-header></my-header>
+  </div>
+</template>
+
+<script>
+import MyHeader from './components/MyHeader'
+
+export default {
+  components: {
+    MyHeader
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div>
+      <h1>京大理学部物理科学系講義Q&amp;A</h1>
+    </div>
+    <div>
+      <p>List</p>
+      <p>Search</p>
+      <div v-if="login">
+        Taro
+      </div>
+      <div v-else>
+        Login
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyHeader',
+  data() {
+        return{
+            login: false
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
